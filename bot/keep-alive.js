886a6ff7951d262be09096234f66ecb47457c5a9
Miccizine <micci.mcr@gmail.com>
2026-05-26 18:55:19 +0800

    Update keep-alive.js
    fix: use https for keep-alive ping when RENDER_EXTERNAL_URL is https in keep-alive.js

--- bot/keep-alive.js
+++ bot/keep-alive.js
@@ -1,12 +1,13 @@
 'use strict';
 const http = require('http');
+const https = require('https');
 
 module.exports = function startKeepAlive() {
   http.createServer((_, res) => res.end('ok')).listen(process.env.PORT || 3000);
   const url = process.env.RENDER_EXTERNAL_URL;
   if (url) {
     setInterval(() => {
-      http.get(url).on('error', () => {});
+      (url.startsWith('https') ? https : http).get(url).on('error', () => {});
     }, 10 * 60 * 1000);
   }
 };
